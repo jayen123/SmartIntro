8cd2e457cca5b81dd291257e2cb58eba0db22668
Jayen Hasan <jayenhasan2003@gmail.com>
2023-10-25 01:49:45 +0600

    motion working fine

--- src/app/card/page.jsx
+++ src/app/card/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+import { useEffect } from "react"
 import ProductCard from "../components/card"
 export default function Card() {
   return (
@@ -5,9 +7,6 @@ export default function Card() {
         <section className="bg-lblack py-[50px]">
         <div className="container">
             <div className="grid grid-cols-1 gap-x-5 gap-y-10 px-[10px] md:px-0 md:grid-cols-3 lg:grid-cols-4">
-                {/* <div >
-
-                </div> */}
                 <ProductCard bg="bg-[url('/images/white-card.png')]" quality="Premium" />
                 <ProductCard bg="bg-[url('/images/red-card.png')]" quality="Premium" />
                 <ProductCard bg="bg-[url('/images/gradient-card.png')]" quality="Premium" />
