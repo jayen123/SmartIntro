1790ce770841edcdb788d600958d91474830af71
jayen123 <jayenhasan2003@gmail.com>
2023-10-28 18:40:53 +0600

    13

--- src/app/components/card.jsx
+++ src/app/components/card.jsx
@@ -2,18 +2,29 @@ import React from 'react'
 import Link from 'next/link'
 import {motion} from "framer-motion"
 export default function ProductCard(props) {
+  const cardVarients = {
+    hidden : {
+      y: 50,
+      scale: 0,
+      opacity: .3
+    },
+    visible : {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      transition :{
+        duration: 1,
+      }
+    }
+  }
   return (
     <>
       <div class="group flex flex-col gap-3">
           <motion.div
-          initial={{
-            x: -100,
-            opacity: .3
-          }}
-          whileInView={{
-            x: 0,
-            opacity: 1,
-          }}
+          variants={cardVarients}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{once: true}}
            className={props.bg + ' bg-no-repeat  bg-cover bg-center aspect-square'}>
               <div class="flex justify-center items-center p-1 opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 w-full h-full bg-lblack/30 transition-all duration-300 ease-in-out">
                   <Link class="btn-primary" href="#">VIEW</Link>
--- src/app/components/commonpage.jsx
+++ src/app/components/commonpage.jsx
@@ -1,11 +1,33 @@
+"use client"
+import { useEffect } from "react"
+import {motion} from "framer-motion"
 const CommonHero = (props) => {
+    const h1Varients = {
+        hidden : {
+            y: 20,
+            opacity: .1
+        },
+        visible : {
+            y: 0,
+            opacity: 1,
+            transition : {
+                duration : 0.5,
+                stiffness : 100
+            }
+        }
+    }
     return(
         <>
             {/* Hero Section */}
             <section className="bg-dblack py-[50px] md:py-[100px]">
                 <div className="container">
                     <div className="row">
-                        <h1 className="bg-gradient-to-b from-lred bg-lpink bg-clip-text text-transparent text-2xl font-bold text-center uppercase md:text-[40px]">{props.heading}</h1>
+                        <motion.h1
+                        variants={h1Varients}
+                        initial="hidden"
+                        animate="visible"
+                        
+                         className="bg-gradient-to-b from-lred bg-lpink bg-clip-text text-transparent text-2xl font-bold text-center uppercase md:text-[40px]">{props.heading}</motion.h1>
                     </div>
                 </div>
             </section>
