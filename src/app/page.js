591fbc373e2aeec74a2d4c0f9425175cf0809ff1
Jayen Hasan <jayenhasan2003@gmail.com>
2023-12-08 01:42:50 +0600

    get api

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import HomeHero from "./components/HomeHero"
 import HomeWork from "./components/HomeWork"
 import HomeFeatures from "./components/HomeFeatures"
-import SingleProductDetails from "./components/singleProductDetails"
 
 
 export default function Home() {
