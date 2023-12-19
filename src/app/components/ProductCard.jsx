f2d642ffdf3edd0a1c6d871e832531984c1db933
Jayen Hasan <jayenhasan2003@gmail.com>
2023-12-19 18:56:47 +0600

    navigation remaining

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,27 +1,55 @@
+"use client"
+import { useState,useEffect } from "react";
 import SingleCard from "./SingleCard";
-export const getCards = async () => {
+import { motion, useInView } from "framer-motion";
+import React, { useRef } from "react";
+
+
+export default function Cards() {
+  // animation
+  const cardVarients = {
+    hidden : {
+      y: 50,
+      opacity: 0
+    },
+    visible : {
+      y: 0,
+      opacity: 1
+    }
+  }
+  // need function
+  const [card, setCard] = useState([])
+  const getCards = async () => {
     let data = await fetch("http://localhost:3000/api/cards");
     data = await data.json();
-    return data;
+    setCard(data);
 } 
+useEffect(() => {
+   getCards();
+}, [])
+const ref = useRef(null);
+const isInview = useInView(ref, {once : true})
 
-
-export default async function Cards() {
-      const cards = await getCards();
+// main code
   return (
     <>
         <section className="bg-lblack py-[50px]">
             <div className="container">
-                <div className="grid grid-cols-1 gap-x-5 gap-y-10 px-[10px] md:px-0 md:grid-cols-3 lg:grid-cols-4">
+                <div ref={ref} className="grid grid-cols-1 gap-x-5 gap-y-10 px-[10px] md:px-0 md:grid-cols-3 lg:grid-cols-4">
                     {
-                      cards.map(({id,image}) => (
-                        <div>
+                      card.map(({id,image},index) => (
+                        <motion.div
+                        variants={cardVarients}
+                        initial="hidden"
+                        animate={isInview ? "visible" : "hidden"}
+                        transition={{ duration: 0.5, delay: index * 0.5 }}
+                        >
                             <SingleCard 
                             key={id}
                             url={`card/${id}`}
                             bg={image}
                             />
-                        </div>
+                        </motion.div>
                       ))
                     }
                 </div>
