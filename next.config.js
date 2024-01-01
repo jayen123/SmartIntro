a61dd9bf4d8e8cc7c54330f0a1db67f02128b9e8
Jayen Hasan <jayenhasan2003@gmail.com>
2024-01-02 01:10:33 +0600

    config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+    future: {
+        webpack5: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.cache = {
+                type: 'filesystem',
+            };
+        }
 
-module.exports = nextConfig
+        return config;
+    },
+};
