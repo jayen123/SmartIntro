c931b5b851fbd5f5d7097c7393f19e15c83a8104
Jayen Hasan <jayenhasan2003@gmail.com>
2024-01-02 00:55:33 +0600

    last commit

--- src/app/components/homeHero.jsx
+++ src/app/components/homeHero.jsx
@@ -1,47 +1,7 @@
 "use client"
 import Link from "next/link"
-import {motion} from "framer-motion"
 
 export default function HomeHero() {
-    const paraVarients = {
-        hidden : {
-            x: 100,
-            opacity:0.3
-        },
-        visible : {
-            x:0,
-            opacity:1,
-            transition : {
-                duration : 1
-            }
-        }
-    };
-    const headingVar = {
-        hidden : {
-            x: 100,
-            opacity:0.2
-        },
-        visible : {
-            x:0,
-            opacity:1,
-            transition : {
-                duration : 1
-            }
-        }
-    };
-    const btnVar = {
-        hidden : {
-            y: 50,
-            opacity:0
-        },
-        visible : {
-            y: 0,
-            opacity:1,
-            transition : {
-                duration : 1
-            }
-        }
-    };
   return (
     <>
         <section className=" flex bg-[url('/images/home-hero.png')] bg-no-repeat bg-center bg-cover py-[50px] md:items-end md:py-0 md:h-[700px]">
@@ -50,27 +10,15 @@ export default function HomeHero() {
                     <div className="px-[10px] md:px-0">
                         <div className="flex flex-col items-end gap-5 md:gap-10 md:mb-32">
                             <div className="font-mont text-white font-bold text-right">
-                                <motion.p
-                                variants={paraVarients}
-                                initial ="hidden"
-                                animate ="visible"
-                                className="text-xs font-bold tracking-wide md:tracking-widest mb-2">SMART INTRO DIGITAL VISITING CARD</motion.p>
-                                <motion.h1
-                                variants={headingVar}
-                                initial ="hidden"
-                                animate ="visible"
-                                className="text-base md:text-5xl">Share Your Contact Info <br/>
-                                With Just A Tap</motion.h1>
+                                <p className="text-xs font-bold tracking-wide md:tracking-widest mb-2">SMART INTRO DIGITAL VISITING CARD</p>
+                                <h1 className="text-base md:text-5xl">Share Your Contact Info <br/>
+                                With Just A Tap</h1>
                             </div>
-                            <motion.div
-                            variants={btnVar}
-                            initial ="hidden"
-                            animate ="visible"
-                            >
-                                <Link href="/">
-                                    <a className="btn-primary">ORDER NOW</a>
+                            <div>
+                                <Link className="btn-primary" href="/">
+                                    ORDER NOW
                                 </Link>
-                            </motion.div>
+                            </div>
                         </div>
                     </div>
                 </div>
