d49dc03ca3f9d6aa8e5e962943aceeddcba9cee7
Jayen Hasan <jayenhasan2003@gmail.com>
2024-01-13 19:08:28 +0600

    get ready to fight

--- src/library/db.jsx
+++ src/library/db.jsx
@@ -1,35 +1,3 @@
-export const users = [
-    {
-        id : 1,
-        name : "Hasan",
-        age : 20,
-        email : "hasan@gmail.com"
-    },
-    {
-        id : 2,
-        name : "Hasib",
-        age : 15,
-        email : "hasib@gmail.com"
-    },
-    {
-        id : 3,
-        name : "Rakib",
-        age : 22,
-        email : "rakib@gmail.com"
-    },
-    {
-        id : 4,
-        name : "Farhan",
-        age : 5,
-        email : "farhan@gmail.com"
-    },
-    {
-        id : 5,
-        name : "Sarika",
-        age : 20,
-        email : "sarika@gmail.com"
-    },
-];
  export const cards = [
     {
       id: 1,
